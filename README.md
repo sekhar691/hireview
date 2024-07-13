8c5574da7eeb614a88fcd7e1bd7bc3fb637ba481
sekhar691 <136946230+sekhar691@users.noreply.github.com>
2024-07-13 18:00:43 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# HireView
+Interviews reimagined: live, interactive, and code-driven.
+
+- Built using ReactJS, [PeerJS](https://peerjs.com/), [react-codemirror](https://www.npmjs.com/package/@uiw/react-codemirror), [Tailwind CSS](https://tailwindcss.com/)
+
+- Uses Online Code Compiler [RapidAPI](https://rapidapi.com/Glavier/api/online-code-compiler/)
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}","./public/index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      }
+    },
+  },
+  plugins: [],
+}
+
